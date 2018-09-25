f8672b51185054cd5ac723e1040c54411d887091
elliothournon <elliothournon@gmail.com>
2018-09-25 11:41:36 -0400

    simple cov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,10 @@
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
-
+  require 'simplecov'
+  SimpleCov.start 'rails'
+  puts "required simplecov"
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
