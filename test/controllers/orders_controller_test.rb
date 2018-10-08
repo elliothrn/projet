140cd9d3d4beb4f78006bbb5cf0289bd06844934
elliothournon <elliothournon@gmail.com>
2018-10-08 13:10:07 -0400

    order

---
+++ test/controllers/orders_controller_test.rb
@@ -0,0 +1,46 @@
+require 'test_helper'
+
+class OrdersControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @order = orders(:one)
+  end
+
+  test "should get index" do
+    get orders_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_order_url
+    assert_response :success
+  end
+
+  test "should create order" do
+    pizza = Pizza.new(name: 'test')
+    pizza.save()
+    assert_difference('Order.count') do
+      post orders_url, params: { order: { name: "test", adress: "here is my adress"}, pizzas:[pizza.id, pizza.id] }
+    end
+
+    assert_redirected_to order_url(Order.last)
+  end
+
+  test "should show order" do
+    get order_url(@order)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_order_url(@order)
+    assert_response :success
+  end
+
+
+  test "should destroy order" do
+    assert_difference('Order.count', -1) do
+      delete order_url(@order)
+    end
+
+    assert_redirected_to orders_url
+  end
+end
